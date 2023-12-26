57fed5d2bfaa43c5b4f8bca4eaf3135ece0e71b4
ntua-el18901 <el18901@mail.ntua.gr>
2023-12-26 19:06:20 +0200

    CLI title added

--- cli-client/cli.js
+++ cli-client/cli.js
@@ -16,4 +16,21 @@ program
     }
   });
 
+program
+  .command('title')
+  .description('Get information about a title by its ID')
+  .option('-i, --titleID <titleID>', 'Specify the title ID')
+  .action(async (options) => {
+    if (options.titleID) {
+      try {
+        const response = await axios.get(`http://localhost:9876/ntuaflix_api/info/title/${options.titleID}`);
+        console.log('Title Information:', response.data);
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    } else {
+      console.error('Please provide a title ID using --titleID.');
+    }
+  });
+
 program.parse(process.argv);
