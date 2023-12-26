ba646b690043deb663ca256fe471f64e296cd058
ntua-el18901 <el18901@mail.ntua.gr>
2023-12-26 19:19:02 +0200

    CLI name added

--- cli-client/cli.js
+++ cli-client/cli.js
@@ -33,4 +33,21 @@ program
     }
   });
 
+program
+  .command('name')
+  .description('Get information about a contributor by its ID')
+  .option('-i, --nameID <nameID>', 'Specify the name ID')
+  .action(async (options) => {
+    if (options.nameID) {
+      try {
+        const response = await axios.get(`http://localhost:9876/ntuaflix_api/info/name/${options.nameID}`);
+        console.log('Contributor Information:', response.data);
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    } else {
+      console.error('Please provide a name ID using --nameID.');
+    }
+  });
+
 program.parse(process.argv);
